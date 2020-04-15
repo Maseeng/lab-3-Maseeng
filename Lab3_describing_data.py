7597f4436ebc8e5dec1491ecb40a7b5121c65fc9
Maseeng <60015243+Maseeng@users.noreply.github.com>
2020-04-14 20:53:25 -0400

    Add files via upload

---
+++ Lab3_describing_data.py
@@ -0,0 +1,15 @@
+myshape = arcpy.Describe ("C:\Users\MMasitha\Downloads\Lab_3_Exploring_Spatial_Data (1)\Lab_3_Exploring_Spatial_Data\Data-Lab_3_Exploring_Spatial_Data")
+myshape.DataType
+mylayer = arcpy.Describe("cities")
+mylayer.DataType
+mylayer.datasetType
+mylayer.catalogPath
+mylayer.basename
+mylayer.file
+mylayer.isVersioned
+mylayer.shapeType
+mylayer.spatialReference
+mylayer.spatialReference.name
+mylayer.spatialReference.type
+mylayer.spatialReference.domain
+
